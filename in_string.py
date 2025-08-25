ea6f55ed6532fe387cb453908f87ae1733faa156
AbrilOrtega2 <aortega2@mail.austral.edu.ar>
2025-08-25 13:08:05 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,6 +1,24 @@
 def check_vowels():
-    # Código a implementar utilizando input.
+   
+
+    txt=input("Ingrese un nombre: ")
+    txt= txt.lower()
+    print("Contiene a: " , "a" in txt)
+    print("Contiene e: " , "e" in txt)
+    print("Contiene i: " , "i" in txt)
+    print("Contiene o: " , "o" in txt)
+    print("Contiene u: ", "u" in txt)
+
+    txt=input("Ingrese un nombre: ")
+    txt= txt.lower()
+
+    print("Contiene a: " , "a" in name)
+    print("Contiene e: " , "e" in name)
+    print("Contiene i: " , "i" in name)
+    print("Contiene o: " , "o" in name)
+    print("Contiene u: ", "u" in name)
 
 
+check_vowels()
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,10 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+    
+    message= input("Ingrese un texto")
+    print(message[4::2])
+    
+    
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+
+
+
