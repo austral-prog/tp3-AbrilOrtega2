4ecc3b43364cc4a2067c7f15bbb1bb54285c45d3
AbrilOrtega2 <aortega2@mail.austral.edu.ar>
2025-09-01 12:00:43 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,12 +1,10 @@
 def check_vowels():
    
 
-    name=input("Ingresa un nombre: ")
+    name=input("Ingrese un nombre: ")
     name= name.lower()
-    print("Contiene a: " , "a" in name)
-    print("Contiene e: " , "e" in name)
-    print("Contiene i: " , "i" in name)
-    print("Contiene o: " , "o" in name)
-    print("Contiene u: " , "u" in name)
-   
-check_vowels()
+    print("Contiene a:" , "a" in name)
+    print("Contiene e:" , "e" in name)
+    print("Contiene i:" , "i" in name)
+    print("Contiene o:" , "o" in name)
+    print("Contiene u:" , "u" in name)
--- slice_simple.py
+++ slice_simple.py
@@ -1,9 +1,12 @@
 def slice_simple():
     texto = "Awesome"
     texto = texto.lower()
+
     nombre = texto [0:3]
     print(nombre)
+
     nombre = texto [2:5]
     print(nombre)
+
     nombre = texto [0:4] + texto [-3:]
     print(nombre)
